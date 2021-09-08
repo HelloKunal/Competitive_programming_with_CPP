c4f50863568b6da3c9708acca8aab24db84370d5
Kunal <hellokunalpawar@gmail.com>
2021-09-09 01:33:10 +0530

    Time: 4 ms (91.06%), Space: 13 MB (86.57%) - LeetHub

---
+++ best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int T_ik0 = 0, T_ik1 = INT_MIN;
+        
+        int T_ik0_old;
+        for(int &price : prices) {
+            T_ik0_old = T_ik0;
+            T_ik0 = max(T_ik0, T_ik1 + price);
+            T_ik1 = max(T_ik1, T_ik0 - price);
+        }
+        
+        return T_ik0;
+    }
+};
