4ad7f8507104e8467a5d9f1cb21980e944ce9e82
Kunal <hellokunalpawar@gmail.com>
2021-10-22 10:27:25 +0530

    Time: 132 ms (19.05%), Space: 5.9 MB (79.45%) - LeetHub

---
+++ permutation-sequence/permutation-sequence.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        string s;
+        for(int i = 1; i <= n; i++) s += i+'0';
+        
+        for(int i = 0; i < k-1; i++) next_permutation(s.begin(), s.end());
+        return s;
+    }
+};
